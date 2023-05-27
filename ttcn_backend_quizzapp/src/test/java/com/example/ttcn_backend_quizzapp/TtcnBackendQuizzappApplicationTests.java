fd15590c0f016f1d346f728558d596e3082187ac
Minh <Luongvanminh12022002@gmail.com>
2023-05-27 21:22:32 +0700

    add backend folder

---
+++ ttcn_backend_quizzapp/src/test/java/com/example/ttcn_backend_quizzapp/TtcnBackendQuizzappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ttcn_backend_quizzapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtcnBackendQuizzappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
